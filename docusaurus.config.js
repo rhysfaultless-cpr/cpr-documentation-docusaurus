198553d85f8ef20ed8fab24fe7d4e58b9fb5cc12
Rhys Faultless <rfaultless@clearpathrobotics.com>
2022-06-09 08:12:08 -0400

    removed Blog section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,9 +26,6 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
         },
-        blog: {
-          showReadingTime: true,
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
