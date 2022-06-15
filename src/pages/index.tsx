4a9f37689c01a680949627c4c29e63564d6691ec
Rhys Faultless <42183066+rhysfaultless-cpr@users.noreply.github.com>
2022-06-15 14:01:11 -0400

    Development (#7)
    - added Prettier for readability
    - addee KaTex equations
    - added content for many Sensors
    - added structure for Accessories
    - added content for Husky user manual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import HomepageMarkdown from '/src/pages/homepage.mdx';
+import HomepageMarkdown from "/src/pages/homepage.mdx";
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <div>
       <Layout
         title={`Home`}
-        description="Description will go into a meta tag in <head />">
-      </Layout>
-      <div>
-        <HomepageMarkdown />
-      </div>
+        description="Description will go into a meta tag in <head />"
+      />
+      <HomepageMarkdown />
     </div>
   );
 }
